9f319cd6a30c874da420eb46a610a4cd79cbfe39
Nikhil Shirahatti <snikhil@me.com>
2012-10-03 23:50:34 -0700

    Not a full, but block level check accomplished

--- src/com/flurry/sudoku/SudokuGrid.java
+++ src/com/flurry/sudoku/SudokuGrid.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SudokuGrid {
 
@@ -85,7 +87,24 @@ public class SudokuGrid {
 			}
 			sb.append("\n");
 		}
+		sb.append("\n");
+		for (SudokuBlock b : blocks) {
+			sb.append(b.toString()).append("\n");
+		}
+
 		return sb.toString();
+	}
+
+	public boolean checkSolved() {
+		boolean solved = true;
+
+		// check blocks
+		for (SudokuBlock b : blocks) {
+			solved &= b.checkSolved();
+		}
+
+		return solved;
+		// check entire table
 	}
 
 	private class SudokuBlock {
@@ -103,32 +122,121 @@ public class SudokuGrid {
 		public SudokuBlock(int[][] grid, int id, int blockSize) {
 			blockId = id;
 			data = new int[blockSize][blockSize];
+			int minc = (id % blockSize) * blockSize;
+			int minr = (id / blockSize) * blockSize;
 
 			for (int r = 0; r < blockSize; r++) {
 				for (int c = 0; c < blockSize; c++) {
-					data[r][c] = grid[id / blockSize * blockSize + r][(id % (blockSize - 1))
-							+ c];
+					data[r][c] = grid[minr + r][minc + c];
 				}
 			}
 
 		}
 
-		public boolean checkCompleted() {
-			return false;
+		public boolean checkSolved() {
+			// 3 rules
+			// row traversal should have a number exactly once
+			// column traversal should have a number exactly once
+			// entire table should have every number between 1-blocksize exactly
+			// once
+
+			boolean solved = true;
+
+			// row check
+			for (int r = 0; r < data.length; r++) {
+				solved &= checkArray(data[r]);
+			}
+
+			// column check
+			int[] colArray = new int[data.length];
+			for (int c = 0; c < data.length; c++) {
+				for (int r = 0; r < data.length; r++) {
+					colArray[r] = data[r][c];
+				}
+
+				solved &= checkArray(colArray);
+			}
+
+			// entire table check
+			return solved &= checkBlock(data);
+
 		}
 
-		@Override
+		private boolean checkBlock(int[][] block) {
+			Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+
+			for (int[] elementArray : block) {
+				for (int c = 0; c < block.length; c++) {
+					int element = elementArray[c];
+					assert (element <= data.length);
+					if (counter.containsKey(element)) {
+						return false;
+					} else {
+						counter.put(element, 1);
+					}
+				}
+
+				assert (counter.values().size() == data.length * data.length);
+				for (int v : counter.values()) {
+					assert (v == 1);
+				}
+
+				return true;
+
+			}
+
+			assert (counter.values().size() == data.length);
+			for (int v : counter.values()) {
+				assert (v == 1);
+			}
+
+			return true;
+		}
+
+		private boolean checkArray(int[] array) {
+			Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+
+			for (int i : array) {
+				assert (i <= data.length);
+				if (counter.containsKey(i)) {
+					return false;
+				} else {
+					counter.put(i, 1);
+				}
+			}
+
+			assert (counter.values().size() == data.length);
+			for (int v : counter.values()) {
+				assert (v == 1);
+			}
+
+			return true;
+		}
+
 		public String toString() {
-			// TODO Auto-generated method stub
-			return String.valueOf(blockId);
+			StringBuilder sb = new StringBuilder();
+
+			sb.append("Data for blockId: ").append(blockId).append("\n");
+			for (int r = 0; r < data.length; r++) {
+				for (int c = 0; c < data.length; c++) {
+					sb.append(data[r][c]);
+					if (c + 1 < data.length) {
+						sb.append(",");
+					}
+				}
+				sb.append("\n");
+			}
+			return sb.toString();
 		}
 
 	}
 
 	public static void main(String[] args) {
 		try {
-			SudokuGrid grid = new SudokuGrid("data/sampleInput_4x4.txt");
+			SudokuGrid grid = new SudokuGrid("data/MangledsampleInput_4x4.txt");
 			System.out.println(grid.toString());
+			System.out.println("Is the given sudoku solved? ".concat(String
+					.valueOf(grid.checkSolved())));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
